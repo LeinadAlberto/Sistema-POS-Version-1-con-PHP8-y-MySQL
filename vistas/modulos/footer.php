0252a6969fda2f08a021ddc11522764f6f0506bf
Alberto <dany.canaviri2022@gmail.com>
2025-03-17 16:22:16 -0400

    Adaptación de AdminLTE al patron MVC y agregación de módulos del sistema finalizado

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+
+    <strong>
+        Copyright &copy; 2025 
+        <a href="https://lokywebdev.com" target="_blank">
+            LokyDevWeb.com
+        </a>
+    </strong>
+
+    - Todos los derechos reservados
+
+</footer>
