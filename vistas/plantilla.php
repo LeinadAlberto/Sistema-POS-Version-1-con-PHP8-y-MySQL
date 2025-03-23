316aea0cb0c7851691de4a70525f567dd82bba63
Alberto <dany.canaviri2022@gmail.com>
2025-03-22 23:44:12 -0400

    Diseño de la página de login finalizado

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -46,71 +46,75 @@
     <!--===========================================
         CUERPO DOCUMENTO
     ============================================-->
-    <body class="hold-transition skin-blue sidebar-collapse sidebar-mini">
-        <!-- Site wrapper -->
-        <div class="wrapper">
+    <body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
+        
+        <?php 
 
-            <?php 
+            if (isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok") {
+
+                echo '<div class="wrapper">';
             
-                /**
-                 * ===========================================
-                 *  CABEZOTE
-                 * ===========================================
-                */
-                include "modulos/cabezote.php";
-
-                /**
-                 * ===========================================
-                 *  MENU
-                 * ===========================================
-                */
-                include "modulos/menu.php";
-
-                /**
-                 * ===========================================
-                 *  CONTENIDO
-                 * ===========================================
-                */
-                if (isset($_GET["ruta"])) {
-
-                    if ($_GET["ruta"] == "inicio" ||
-                        $_GET["ruta"] == "usuarios" ||
-                        $_GET["ruta"] == "categorias" ||
-                        $_GET["ruta"] == "productos" ||
-                        $_GET["ruta"] == "clientes" ||
-                        $_GET["ruta"] == "ventas" ||
-                        $_GET["ruta"] == "crear-venta" ||
-                        $_GET["ruta"] == "reportes" 
-                    ) {
-
-                        include "modulos/" . $_GET["ruta"] . ".php";
-
+                    /**
+                     * ===========================================
+                     *  CABEZOTE
+                     * ===========================================
+                    */
+                    include "modulos/cabezote.php";
+    
+                    /**
+                     * ===========================================
+                     *  MENU
+                     * ===========================================
+                    */
+                    include "modulos/menu.php";
+    
+                    /**
+                     * ===========================================
+                     *  CONTENIDO
+                     * ===========================================
+                    */
+                    if (isset($_GET["ruta"])) {
+    
+                        if ($_GET["ruta"] == "inicio" ||
+                            $_GET["ruta"] == "usuarios" ||
+                            $_GET["ruta"] == "categorias" ||
+                            $_GET["ruta"] == "productos" ||
+                            $_GET["ruta"] == "clientes" ||
+                            $_GET["ruta"] == "ventas" ||
+                            $_GET["ruta"] == "crear-venta" ||
+                            $_GET["ruta"] == "reportes" 
+                        ) {
+    
+                            include "modulos/" . $_GET["ruta"] . ".php";
+    
+                        } else {
+    
+                            include "modulos/404.php";
+    
+                        }
+    
                     } else {
-
-                        include "modulos/404.php";
-
+    
+                        include "modulos/inicio.php";
+    
                     }
+                    
+                    /**
+                     * ===========================================
+                     *  FOOTER
+                     * ===========================================
+                    */
+                    include "modulos/footer.php";
+    
+                echo '</div>'; # /.wrapper 
 
-                } else {
+            } else {
 
-                    include "modulos/inicio.php";
+                include "modulos/login.php";
 
-                }
-                
-                /**
-                 * ===========================================
-                 *  FOOTER
-                 * ===========================================
-                */
-                include "modulos/footer.php";
+            }
 
-                
-            
-            ?>
-
-            
-
-        </div><!-- ./wrapper -->
+        ?>
 
         <script src="vistas/js/plantilla.js"></script>
 
